d36f60655a447ac91a6469969d42315c2c0853e6
Emmanuel Obakpolor <emmanuelobakpolor6655@gmail.com>
2026-06-23 10:17:57 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+// TODO: Uncomment when client pays
+// import LandingPage from './components/LandingPage/LandingPage'
+
+export default function App() {
+  // TODO: Uncomment when client pays
+  // return <LandingPage />
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+        fontFamily: 'system-ui, -apple-system, sans-serif',
+        background: '#fafafa',
+        color: '#333',
+        textAlign: 'center',
+        padding: '2rem',
+      }}
+    >
+      <h1 style={{ fontSize: '6rem', margin: 0, fontWeight: 700, color: '#222' }}>404</h1>
+      <p style={{ fontSize: '1.25rem', color: '#666', marginTop: '0.5rem' }}>
+        This page could not be found.
+      </p>
+    </div>
+  )
+}
